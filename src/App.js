e4db52eece56d2ba22f65634db30a86a5d8744c5
pradyunx21 <pradyunsandhu987@icloud.com>
2024-09-15 16:25:33 +0530

    Task 4.1P

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
 import './App.css';
+import CardList from './CardList';
+import Main_Photo from './mainimg';
+import SearchBar from './searchbar';
+import Mid_section from './mid_section';
+import BottomBar from './bottomsection';
+import Top_section from './top_section';
+import Btm_section from './featured_Tut';
+import Mid_section2 from './mid_section2';
+import footer from './Footer';
+import CardList2 from './MyTutorial';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <SearchBar/>   
+      <Main_Photo/>
+      <Top_section/>
+      <CardList />
+      <Mid_section />
+      <CardList2/>
+      <Btm_section/>
+      <Mid_section2/>
+      <BottomBar/>
     </div>
   );
 }
